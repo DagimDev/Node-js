e3c53dac0673430f5658e0822072469a3880ae81
DagimDev <simedagim121@gmail.com>
2025-07-18 23:10:43 +0300

    Initialize path module basics

---
+++ Node_Modules_Core/path-module/1-path-basics.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+console.log('Path separator:', path.sep); // '\' on Windows, '/' on Unix
+console.log('Current file path:', __filename);
+console.log('Current directory:', __dirname);
